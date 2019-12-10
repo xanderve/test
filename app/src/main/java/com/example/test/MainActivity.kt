c68d16394bde3bee8dc1037601432e706d9faa11
Xander <xander@xandervanerven.nl>
2019-12-10 14:54:46 +0100

    spotify code added playing song in the background //Nearby.getMessagesClient(this).unpublish(message!!) //Nearby.getMessagesClient(this).unsubscribe(messageListener!!) nog uitgezet want zorgt voor nullable error die ik nog niet kan oplossen

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -9,9 +9,23 @@ import com.google.android.gms.nearby.messages.Message
 import com.google.android.gms.nearby.messages.MessageListener
 import kotlinx.android.synthetic.main.activity_main.*
 
+//spotify
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
 class MainActivity : AppCompatActivity() {
     private var messageListener: MessageListener? = null
     private var message: Message? = null
+
+    private val clientId = "e0a722ced90449768ad5bc5f85561c14"
+    private val redirectUri = "http://com.example.test/callback"
+    private var spotifyAppRemote: SpotifyAppRemote? = null
+
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,12 +61,44 @@ class MainActivity : AppCompatActivity() {
         //Toast.makeText(this@MainActivity, "onstart", Toast.LENGTH_SHORT).show()
         //Nearby.getMessagesClient(this).publish(mMessage!!)
         Nearby.getMessagesClient(this).subscribe(messageListener!!)
+
+        val connectionParams = ConnectionParams.Builder(clientId)
+                .setRedirectUri(redirectUri)
+                .showAuthView(true)
+                .build()
+
+        SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
+            override fun onConnected(appRemote: SpotifyAppRemote) {
+                spotifyAppRemote = appRemote
+                Log.d("MainActivity", "Connected! Yay!")
+                // Now you can start interacting with App Remote
+                connected()
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                Log.e("MainActivity", throwable.message, throwable)
+                // Something went wrong when attempting to connect! Handle errors here
+            }
+        })
+
+    }
+
+    private fun connected() {
+        // Then we will write some more code here.
+        spotifyAppRemote?.let {
+            // Then we will write some more code here.
+            val playlistURI = "spotify:playlist:37i9dQZF1DX2sUQwD7tbmL"
+            it.playerApi.play(playlistURI)
+        }
     }
 
     public override fun onStop() {
         //Toast.makeText(this@MainActivity, "onstop", Toast.LENGTH_SHORT).show()
-        Nearby.getMessagesClient(this).unpublish(message!!)
-        Nearby.getMessagesClient(this).unsubscribe(messageListener!!)
         super.onStop()
+        //Nearby.getMessagesClient(this).unpublish(message!!)
+        //Nearby.getMessagesClient(this).unsubscribe(messageListener!!)
+        spotifyAppRemote?.let {
+            SpotifyAppRemote.disconnect(it)
+        }
     }
 }
